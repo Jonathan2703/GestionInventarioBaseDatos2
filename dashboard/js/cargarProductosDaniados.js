ce1f5330e817920ce8e28c94b4d23a2a5eaf4aee
Jonathan2703 <jonnathan.valdez@ucuenca.edu.ec>
2020-07-13 04:19:37 -0500

    respaldo

--- dashboard/js/cargarProductosDaniados.js
+++ dashboard/js/cargarProductosDaniados.js
@@ -1,5 +1,4 @@
-
-var request = new Request('https://heroku-demochris.herokuapp.com/api/producto/1/estado?estado=2');
+var request = new Request('https://heroku-demochris.herokuapp.com/api/producto/1/estado?estado=3');
 productosCaducados()
 function productosCaducados() {
     console.log('productos ')
@@ -9,7 +8,7 @@ function productosCaducados() {
             $('#Table').append(
                 '<tr> <th scope="col">Id Producto</th>' +
                 '<th scope="col">Nombre Producto</th>' +
-                '<th scope="col">Categoria</th>' +
+                '<th scope="col">Descuento</th>' +
                 '<th scope="col">Stock</th>' +
                 '<th scope="col">Fecha Caducidad</th>' +
                 '<th scope="col">Ubicacion</th>' +
@@ -20,10 +19,11 @@ function productosCaducados() {
             }
             for (var i = 0; i < data.length; i++) {
                 console.log(data[i].id_producto)
+                
                 $('#Table').append(
                     ' <tr><th scope="row">' + data[i].id + '</th>' +
                     ' <td>' + data[i].nombre + '</td>' +
-                    '<td>' + data[i].categoria.nombre_categoria + '</td>' +
+                    '<td>' + data[i].descuento+'%'+ '</td>' +
                     '<td>' + data[i].stock_producto + '</td>' +
                     '<td>' + data[i].fecha_expiracion_producto + '</td>' +
                     '<td>' + data[i].id_seccion_producto.id_ubicacion_producto.descripcion + '</td>' +
